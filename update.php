8bdd678195727a618173c5b022836f76a94b960e
Jirayu Kanda <me@acodify.com>
2016-07-04 17:04:45 +0700

    fix bug path

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include __DIR__.'/../inc.php';
+include __DIR__.'/inc.php';
 
 if ( empty( $_GET['type'] ) ) {
     header('Location: '.ABSURL); 
